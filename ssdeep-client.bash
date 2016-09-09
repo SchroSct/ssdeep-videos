780d1f0236db8815de966ece3d9a324fe19f2261
SchroSct <clls760@gmail.com>
2016-09-08 20:56:04 -0700

    Update ssdeep-client.bash
    There's a math operand error in there, but it's not the worst.

--- ssdeep-client.bash
+++ ssdeep-client.bash
@@ -1,9 +1,33 @@
 #!/bin/bash
 dname="$1"
 locbase=""
-mapfile -t matches < <(ssh $user@$server ssdeep-date.bash "$dname")
+mapfile -t matches < <(ssh user@server ssdeep-all-compare.bash "$dname")
 let n=0
-tmp=$(mktemp -d)
+mkdir /foo/bar/
+tmp=/foo/bar/
+function mflist(){
+      for line in "${matches[@]}"
+      do
+         f1=$(echo "$line" | grep "$(basename ${file1})" | cut -d" " -f1)
+         f2=$(echo "$line" | grep "$(basename ${file1})" | cut -d" " -f2)
+         cha=$(echo "$line" | grep "$(basename ${file1})" | cut -d" " -f3)
+         if [[ "$f1" != "$f2" ]] && [[ -r "${locbase}${f1}" ]] && [[ -r "${locbase}${f2}" ]]
+         then
+            echo "$f1"
+            echo "$f2"
+         fi
+      done
+}
+function chlist(){
+      let chan=0
+      for line in "${matches[@]}"
+      do
+         cha=$(echo "$line" | grep "$(basename ${file1})" | cut -d" " -f3)
+         let chan=$chan+$cha
+      done
+      echo "$chan"
+}
+
 for match in "${matches[@]}"
 do
    file1=$(echo "$match" | cut -d" " -f1)
@@ -18,100 +42,55 @@ do
       echo "One of the files is not readable, skipping."
       let n=$n+1
    else
-      cp -vpn "${locbase}${file1}" "$tmp"
-      cp -vpn "${locbase}${file2}" "$tmp"
-      echo "Files Copied to $tmp"
-      for line in "${matches[@]}"
+      mapfile -t files < <(mflist | sort | uniq )
+      let chance=$(chlist)
+      let filenum=0
+      for file in "${files[@]}"
       do
-         f1=$(echo "$line" | grep "$(basename ${file1})" | cut -d" " -f1)
-         f2=$(echo "$line" | grep "$(basename ${file1})" | cut -d" " -f2)
-         cha=$(echo "$line" | grep "$(basename ${file1})" | cut -d" " -f3)
-         if [[ "$f1" != "$f2" ]]
-         then
-            echo "File1: $f1 $f2 $cha"
-         fi
-      done
-      for line in "${matches[@]}"
-      do
-         f1=$(echo "$line" | grep "$(basename ${file2})" | cut -d" " -f1)
-         f2=$(echo "$line" | grep "$(basename ${file2})" | cut -d" " -f2)
-         cha=$(echo "$line" | grep "$(basename ${file2})" | cut -d" " -f3)
-         if [[ "$f1" != "$f2" ]]
-         then
-            echo "File2: $f1 $f2 $cha"
-         fi
+         echo "File $filenum is ${files[$filenum]}"
+         cp -pn "${locbase}${file}" "$tmp"
+         ls -lah "${locbase}$file"
+         echo "$(echo "$(ffprobe "${tmp}/$(basename ${file})" 2>&1 | grep -i duration | sed -e 's/.*Duration: //g' -e 's/,.*//g' | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') * .${chance}" | bc) seconds overlap"
       done
       echo "Chance of a match is ${chance}%"
-      echo "File 1 is $file1"
-      echo "File 2 is $file2"
-      ls -lah "${locbase}$file1"
-      ls -lah "${locbase}$file2"
-      echo "$(echo "$(ffprobe "${tmp}/$(basename ${file1})" 2>&1 | grep -i duration | sed -e 's/.*Duration: //g' -e 's/,.*//g' | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') * .${chance}" | bc) seconds overlap"
-      echo "$(echo "$(ffprobe "${tmp}/$(basename ${file2})" 2>&1 | grep -i duration | sed -e 's/.*Duration: //g' -e 's/,.*//g' | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') * .${chance}" | bc) seconds overlap"
       echo "Press Return to start comparison"
       read nothing
+      let mpvnum=0
+      for file in "${files[@]}"
+      do
+         mpv --pause --no-audio --loop=inf --speed=1.33 "${tmp}/$(basename ${file})" &>/dev/null &
+         mpvpid[$mpvnum]=$!
+         let mpvnum=$mpvnum+1
+      done
       sleep 1.5
-      WIDTH=`xdpyinfo | grep 'dimensions:' | cut -f 2 -d ':' | cut -f 1 -d 'x'`;
-      HALF=$(($WIDTH/2));
-      mpv --no-audio --loop=inf --speed=1.33 "${tmp}/$(basename ${file1})" &>/dev/null &
-      mpv1=$!
-      mpv --no-audio --loop=inf --speed=1.33 "${tmp}/$(basename ${file2})" &>/dev/null &
-      mpv2=$!
-      until wmctrl -l -p | grep -i "$mpv1" &>/dev/null || wmctrl -l -p | grep -i "$mpv2" &>/dev/null
+      let lastmpv=${#mpvpid}-1
+      until wmctrl -l -p | grep -i "${mpvpid[0]}" &>/dev/null || wmctrl -l -p | grep -i "$mpvpid[${lastmpv}]" &>/dev/null
       do
          sleep .5
       done
-      wid=$(wmctrl -l -p | grep -i "$mpv1" | sed -e 's/\ .*//g')
-      wmctrl -r "$wid" -i -b add,maximized_vert && wmctrl -r "$wid" -i -e 0,0,0,$HALF,-1
-      wid=$(wmctrl -l -p | grep -i "$mpv2" | sed -e 's/\ .*//g')
-      wmctrl -r "$wid" -i -b add,maximized_vert && wmctrl -r "$wid" -i -e 0,$HALF,0,$HALF,-1
-      select opt in $(echo "KeepFile1" ; echo "KeepFile2" ; echo "KeepBoth" ; echo "RemoveBoth")
+      sleep 2
+      rearrange.py mpv
+      select opt in $(for t in "${files[@]}" ; do echo "$t" ; done ; echo "None" )
       do
-         echo "You chose $opt"
-         case $opt in
-            "KeepFile1")
-               echo "Keeping $file1"
-               rm -v "${locbase}${file2}"
-               rm -v "${tmp}/$(basename $file2)"
-               break
-               ;;
-            "KeepFile2")
-               echo "Keeping $file2"
-               rm -v "${locbase}${file1}"
-               rm -v "${tmp}/$(basename $file1)"
-               break
-               ;;
-            "KeepBoth")
-               echo "Keeping $file1 & $file2"
-               break
-               ;;
-            "RemoveBoth")
-               echo "Removing $file1 & $file2"
-               rm -v "${locbase}${file1}"
-               rm -v "${tmp}/$(basename $file1)"
-               rm -v "${locbase}${file2}"
-               rm -v "${tmp}/$(basename $file2)"
-               break
-               ;;
-
+          case "$opt" in
+             "None")
+                echo "Deleting None"
+                break
+                ;;
              *)
-               break
-               ;;
+                echo "Deleting $opt"
+                read nothing
+                rm -v "${locbase}${opt}"
+                rm -v "${tmp}/$(basename $opt)"
+                break
+                ;;
          esac
       done
-      kill -- "$mpv1"
-      kill -- "$mpv2"
+      for i in "${mpvpid[@]}"
+      do
+         kill -- "$i"
+      done
       let n=n+1
-      nfile1=$(echo "${matches[$n]}" | cut -d" " -f1)
-      nfile2=$(echo "${matches[$n]}" | cut -d" " -f2)
-      if [[ "$nfile1" != "$file1" ]] && [[ "$nfile2" != "$file1" ]]
-      then
-         rm -v "${tmp}/$(basename $file1)"
-      fi
-      if [[ "$nfile1" != "$file2" ]] && [[ "$nfile2" != "$file2" ]]
-      then
-         rm -v "${tmp}/$(basename $file2)"
-      fi
       clear
    fi
 done
